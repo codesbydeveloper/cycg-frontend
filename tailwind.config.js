991c0035563a3b61c0c48830825db0e9fb3a822a
Chaitanyarana02 <chaitanyaranaa02@gmail.com>
2026-01-27 16:22:49 +0530

    cycg home

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kiona: ['Kiona', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
